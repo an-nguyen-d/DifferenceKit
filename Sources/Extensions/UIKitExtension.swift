9426c9010cf6f45917224b3cd739efbf945a6313
An Nguyen <annguyen@knights.ucf.edu>
2024-01-31 01:44:26 +0900

    Add completion to reload

--- Sources/Extensions/UIKitExtension.swift
+++ Sources/Extensions/UIKitExtension.swift
@@ -137,58 +137,77 @@ public extension UICollectionView {
     ///                updates should be stopped and performed reloadData. Default is nil.
     ///   - setData: A closure that takes the collection as a parameter.
     ///              The collection should be set to data-source of UICollectionView.
-    func reload<C>(
-        using stagedChangeset: StagedChangeset<C>,
-        interrupt: ((Changeset<C>) -> Bool)? = nil,
-        setData: (C) -> Void
-        ) {
-        if case .none = window, let data = stagedChangeset.last?.data {
-            setData(data)
-            return reloadData()
-        }
-
-        for changeset in stagedChangeset {
-            if let interrupt = interrupt, interrupt(changeset), let data = stagedChangeset.last?.data {
-                setData(data)
-                return reloadData()
-            }
-
-            performBatchUpdates({
-                setData(changeset.data)
-
-                if !changeset.sectionDeleted.isEmpty {
-                    deleteSections(IndexSet(changeset.sectionDeleted))
-                }
-
-                if !changeset.sectionInserted.isEmpty {
-                    insertSections(IndexSet(changeset.sectionInserted))
-                }
-
-                if !changeset.sectionUpdated.isEmpty {
-                    reloadSections(IndexSet(changeset.sectionUpdated))
-                }
-
-                for (source, target) in changeset.sectionMoved {
-                    moveSection(source, toSection: target)
-                }
-
-                if !changeset.elementDeleted.isEmpty {
-                    deleteItems(at: changeset.elementDeleted.map { IndexPath(item: $0.element, section: $0.section) })
-                }
-
-                if !changeset.elementInserted.isEmpty {
-                    insertItems(at: changeset.elementInserted.map { IndexPath(item: $0.element, section: $0.section) })
-                }
-
-                if !changeset.elementUpdated.isEmpty {
-                    reloadItems(at: changeset.elementUpdated.map { IndexPath(item: $0.element, section: $0.section) })
-                }
-
-                for (source, target) in changeset.elementMoved {
-                    moveItem(at: IndexPath(item: source.element, section: source.section), to: IndexPath(item: target.element, section: target.section))
-                }
-            })
-        }
+  func reload<C>(
+    using stagedChangeset: StagedChangeset<C>,
+    interrupt: ((Changeset<C>) -> Bool)? = nil,
+    setData: (C) -> Void,
+    completion: (() -> Void)? = nil
+  ) {
+    if case .none = window, let data = stagedChangeset.last?.data {
+      setData(data)
+      reloadData()
+      performBatchUpdates(nil, completion: {
+        (result) in
+        completion?()
+      })
+      return
     }
+
+    let updatesCount = stagedChangeset.count
+    var completedUpdates = 0
+
+    for changeset in stagedChangeset {
+      if let interrupt = interrupt, interrupt(changeset), let data = stagedChangeset.last?.data {
+        setData(data)
+        reloadData()
+        performBatchUpdates(nil, completion: {
+          (result) in
+          completion?()
+        })
+        return
+      }
+
+      performBatchUpdates({
+        setData(changeset.data)
+
+        if !changeset.sectionDeleted.isEmpty {
+          deleteSections(IndexSet(changeset.sectionDeleted))
+        }
+
+        if !changeset.sectionInserted.isEmpty {
+          insertSections(IndexSet(changeset.sectionInserted))
+        }
+
+        if !changeset.sectionUpdated.isEmpty {
+          reloadSections(IndexSet(changeset.sectionUpdated))
+        }
+
+        for (source, target) in changeset.sectionMoved {
+          moveSection(source, toSection: target)
+        }
+
+        if !changeset.elementDeleted.isEmpty {
+          deleteItems(at: changeset.elementDeleted.map { IndexPath(item: $0.element, section: $0.section) })
+        }
+
+        if !changeset.elementInserted.isEmpty {
+          insertItems(at: changeset.elementInserted.map { IndexPath(item: $0.element, section: $0.section) })
+        }
+
+        if !changeset.elementUpdated.isEmpty {
+          reloadItems(at: changeset.elementUpdated.map { IndexPath(item: $0.element, section: $0.section) })
+        }
+
+        for (source, target) in changeset.elementMoved {
+          moveItem(at: IndexPath(item: source.element, section: source.section), to: IndexPath(item: target.element, section: target.section))
+        }
+      }, completion: { _ in
+        completedUpdates += 1
+        if completedUpdates == updatesCount {
+          completion?()  // Call the completion handler only after all updates are finished
+        }
+      })
+    }
+  }
 }
 #endif
